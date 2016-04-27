db7a6d09ae0e3b1580fef60f55d34e64149d6212
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-27 10:07:30 -0400

    Broke my game ... Will fix it later.

--- ttt.js
+++ ttt.js
@@ -1,37 +1,49 @@
 $(document).on('ready', function() {
   var turn = 0;
   var winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,9]];
+  startGame();
 
   function checkWinner() {
-    // check for a winner here
     for(var i = 0; i < winningCombos.length; i++) {
       var combo = winningCombos[i];
 
       if ($('.x').hasClass(combo[0]) && $('.x').hasClass(combo[1]) && $('.x').hasClass(combo[2])) {
-        alert("X wins!!")
-        return
+        alert("X wins!!");
+        refreshGame();
+        return true
       } else if ($('.o').hasClass(combo[0]) && $('.o').hasClass(combo[1]) && $('.o').hasClass(combo[2])) {
-        alert("O wins!!")
-        return
-      // } else if (turn === 9) {
-      //   alert("Game Over..")
+        alert("O wins!!");
+        refreshGame();
+        return true
       };
-
+      return false
     };
+  function refreshGame() {
+      $('td').removeClass('x').removeClass('o').html("");
+      turn = 0;
+      startGame();
   };
 
-  $('td').on('click', function () {
-    if (turn % 2 === 0) {
-      $(this).html('X').addClass('x');
-    } else {
-      $(this).html('O').addClass('o');
-    }
-
-    checkWinner();
+  function gameOver() {}
+    if (turn === 9 && (checkWinner () === false)) {
+      alert("Game Over!");
+      refreshGame()
+    };
+  };
+  function startGame() {
+    $('td').on('click', function () {
+      if (turn % 2 === 0) {
+        $(this).html('X').addClass('x');
+      } else {
+        $(this).html('O').addClass('o');
+      }
 
     $(this).off('click');
-    turn++
-  });
+    turn++;
 
+      checkWinner();
+      // refreshGame();
+    });
+  };
 
 });
